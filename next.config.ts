38ee7c9d7c92fb3ce3242b03c3795ef545a8e97d
Uksharma2611 <mysrms2021@gmail.com>
2026-04-29 14:44:46 +0530

    fixed a few bugs

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // THE FIX: Tells Next.js to compile a standalone server for cPanel
+  output: "standalone",
+  
   // Tells Next.js to ignore strict TypeScript errors when deploying
   typescript: {
     ignoreBuildErrors: true,
